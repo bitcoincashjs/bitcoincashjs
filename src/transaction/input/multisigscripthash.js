bbb33f4f1bd75067408fb6bb53f321408266ff4e
Clemens Ley <ley.clemens@gmail.com>
2018-06-29 16:43:56 -0700

    minor

--- src/transaction/input/multisigscripthash.js
+++ src/transaction/input/multisigscripthash.js
@@ -25,7 +25,7 @@ function MultiSigScriptHashInput(input, pubkeys, threshold, signatures, redeemSc
   this.publicKeys = _.sortBy(pubkeys, publicKey => publicKey.toString('hex'));
   this.redeemScript = redeemScript || Script.buildMultisigOut(this.publicKeys, this.threshold);
   $.checkState(Script.buildScriptHashOut(this.redeemScript).equals(this.output.script),
-    'RedeemScript don\'t hash to the provided output'
+    'RedeemScript does not hash to the provided output'
   );
   this.publicKeyIndex = {};
   this.publicKeys.forEach((publicKey, index) =>
