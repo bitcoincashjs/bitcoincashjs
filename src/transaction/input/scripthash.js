c94708179c20260425871d14f6ddd27648863b3a
Clemens Ley <ley.clemens@gmail.com>
2018-07-05 14:52:55 -0700

    Fixed another bug in ScriptHashInput
    It should be this.publicKeys not this.pubkeys

--- src/transaction/input/scripthash.js
+++ src/transaction/input/scripthash.js
@@ -19,7 +19,7 @@ var TransactionSignature = require('../signature');
 function ScriptHashInput(input, pubkeys, redeemScript) {
   Input.apply(this, arguments);
   var self = this;
-  this.pubkeys = pubkeys || input.publicKeys;
+  this.publicKeys = pubkeys || input.publicKeys;
   this.redeemScript = redeemScript;
   $.checkState(Script.buildScriptHashOut(this.redeemScript).equals(this.output.script),
     'Provided redeemScript doesn\'t hash to the provided output');
