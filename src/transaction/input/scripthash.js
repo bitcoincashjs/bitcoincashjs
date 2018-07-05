367259a21589cf15f486f96e2d761a86f86598ae
Clemens Ley <ley.clemens@gmail.com>
2018-07-05 15:28:16 -0700

    fixed another bug in ScriptHashInput
    this.threshold was never set

--- src/transaction/input/scripthash.js
+++ src/transaction/input/scripthash.js
@@ -20,6 +20,7 @@ function ScriptHashInput(input, pubkeys, redeemScript) {
   Input.apply(this, arguments);
   var self = this;
   this.publicKeys = pubkeys || input.publicKeys;
+  this.threshold = threshold || input.threshold;
   this.redeemScript = redeemScript;
   $.checkState(Script.buildScriptHashOut(this.redeemScript).equals(this.output.script),
     'Provided redeemScript doesn\'t hash to the provided output');
